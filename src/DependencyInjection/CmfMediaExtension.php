145016740a232806c8f98aa2738020d6192dd15e
Xavier Lembo <xlembo@eliberty.fr>
2024-02-26 16:39:06 +0100

    pass controllers as public for sf4

--- src/DependencyInjection/CmfMediaExtension.php
+++ src/DependencyInjection/CmfMediaExtension.php
@@ -177,6 +177,9 @@ class CmfMediaExtension extends Extension implements PrependExtensionInterface
             if (false !== $config['event_listeners']['imagine_cache']) {
                 $loader->load('persistence-phpcr-event-imagine.yml');
             }
+            $definition = $container->getDefinition($this->getAlias().'.templating.helper');
+            $definition->addArgument(new Reference('liip_imagine.templating.filter_helper'));
+            
         } elseif (true === $config['event_listeners']['imagine_cache']) {
             throw new InvalidConfigurationException('persistence.phpcr.event_listeners.imagine_cache may not be forced enabled if Imagine is not enabled.');
         }
